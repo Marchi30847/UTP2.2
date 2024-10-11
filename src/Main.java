2b3e4ba087127b3625f7adcca38225f34d7a44d2
Danial <dmunatayev.dev@gmail.com>
2024-10-11 18:08:39 +0200

    Corrected junior mistake

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        //TODO: SOSI HUI
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+
+        Subtractor subtractor = new Subtractor();
+        System.out.println(subtractor.subtract(6, 3));
+
+        Multiplier multiplier = new Multiplier();
+        System.out.println(multiplier.multiply(6, 3));
+
+        Dividor dividor = new Dividor();
+        System.out.println(dividor.divide(6, 3));
     }
 }
