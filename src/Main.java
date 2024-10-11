e091ecbbab164525c0fbd9c376454e66549b5b41
Marchi30847 <s30847@pjwstk.edu.pl>
2024-10-11 18:03:39 +0200

    Sosat

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@
 //OK, I will add the Adder and s30846 will add the Subtractor
 
 public class Main {
-    public static void main(String args) {
+    public static void main(String[] args) {
         Adder adder = new Adder();
         System.out.println(adder.add(1, 2));
 
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
+
+        Multiplier multiplier = new Multiplier();
+        System.out.println(multiplier.multiply(6, 3));
+
+        Dividor dividor = new Dividor();
+        System.out.println(dividor.divide(6, 3));
     }
 }
