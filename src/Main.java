1449b96480ca87566da1dfc8a8dbd8ffba32dccb
s30050Danny <s30050@pjwstk.edu.pl>
2024-10-11 18:05:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
-
-        Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
-
-        Multiplier multiplier = new Multiplier();
-        System.out.println(multiplier.multiply(6, 3));
-
-        Dividor dividor = new Dividor();
-        System.out.println(dividor.divide(6, 3));
+        //TODO: SOSI HUI
     }
 }
